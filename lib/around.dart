d983f1cb68f9b67b35dae52a6dd8ae6f51c68570
JONATHAN_ACEVEDO_CASTRO <a.21308051280425@cbtis128.edu.mx>
2024-04-11 16:11:02 +0000

    asd

---
+++ lib/around.dart
@@ -0,0 +1,42 @@
+import 'package:acevedodrawer2/drawer_menu.dart';
+import 'package:flutter/material.dart';
+
+class Around extends StatelessWidget {
+  static const String routeName = '/around';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xffcaf0f8),
+        title: Text("Around Jonathan Acevedo"),
+      ),
+      drawer: DrawerMenu(),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(0.0),
+            color: Color(0xff90e0ef),
+            width: 80.0,
+            height: 80.0,
+          ),
+          Container(
+            padding: const EdgeInsets.all(0.0),
+            color: Color(0xff48cae4),
+            width: 80.0,
+            height: 80.0,
+          ),
+          Container(
+            padding: const EdgeInsets.all(0.0),
+            color: Color(0xff00b4d8),
+            width: 80.0,
+            height: 80.0,
+          ),
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/stretch.dart
@@ -0,0 +1,42 @@
+import 'package:acevedodrawer2/drawer_menu.dart';
+import 'package:flutter/material.dart';
+
+class Stretch extends StatelessWidget {
+  static const String routeName = '/stretch';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xffcaf0f8),
+        title: Text("Stretch Jonathan Acevedo"),
+      ),
+      drawer: DrawerMenu(),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(0.0),
+            color: Color(0xff90e0ef),
+            width: 80.0,
+            height: 80.0,
+          ),
+          Container(
+            padding: const EdgeInsets.all(0.0),
+            color: Color(0xff48cae4),
+            width: 80.0,
+            height: 80.0,
+          ),
+          Container(
+            padding: const EdgeInsets.all(0.0),
+            color: Color(0xff00b4d8),
+            width: 80.0,
+            height: 80.0,
+          ),
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
